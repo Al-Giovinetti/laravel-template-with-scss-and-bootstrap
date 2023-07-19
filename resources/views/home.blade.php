43d5374465c4f9145bf1c75b214bfc4694cbc5e0
Al-Giovinetti <giovinettialessio@gmail.com>
2023-07-19 19:41:07 +0200

    Laravel template with simple content

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends("layout.app")
+
+@section("title","Home")
+
+@section("main")
+<p> Parte centrale </p>
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <h1>Titolo X</h1>
+    <nav>
+        <ul>
+            <li>A</li>
+            <li>B</li>
+            <li>C</li>
+        </ul>
+    </nav>
+</header>
